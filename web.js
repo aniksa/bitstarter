89127fc3f48141e2aadd8303f99eb5c66625fcb1
aniksa <aniksa@gmail.com>
2013-07-07 11:05:17 +0300

    web.js
    error fix

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs=require('fs');
   var infile = "index.html";
-  var buffer = fs.readFile(outfile);
+  var buffer = fs.readFile(infile);
   response.send(buffer.toString());
 });
 
